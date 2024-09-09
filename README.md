67e030a9554ebfb2336f3abbc947e07661aa0594
Nixonw48 <nixonwongzijie@gmail.com>
2024-09-09 15:13:35 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://nixonw48.github.io/despacito/](https://nixonw48.github.io/despacito/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/nixonw48/despacito** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/nixonw48/despacito** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.py
@@ -0,0 +1,246 @@
+def on_button_pressed_a():
+    basic.show_string("DESPASITO")
+    basic.show_icon(IconNames.HEART)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(370, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(659, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(659, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(659, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(370, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(659, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.DOUBLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.DOUBLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(370, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(370, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(370, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(370, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(370, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(523, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(494, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(659, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(659, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(554, music.beat(BeatFraction.DOUBLE)),
+        music.PlaybackMode.UNTIL_DONE)
+input.on_button_pressed(Button.A, on_button_pressed_a)
